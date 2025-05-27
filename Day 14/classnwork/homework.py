da5b2cb7b6f1643418fd92856a4621b0075f3fee
demetresuxitashvili <tamunichko@gmail.com>
2025-05-27 19:43:20 +0400

    second commit

---
+++ Day 14/classnwork/homework.py
@@ -0,0 +1,22 @@
+# 1
+# <, >, <=, >=, ==, !=
+
+# 2 
+# and - და
+# or - ან 
+
+# 3 
+# and - და
+print(True and True) # output: True
+print(True and False) # output: False
+print(False and True) # output: False
+print(False and False) # output: False
+
+# and ოპერატორის გამოყენების დროს, მხოლოდ და მხოლოდ მაშინ იქნება ლოგიკური ოპერაციის შედეგი True, თუ ორივე პირობა True-ს ტოლი იქნება. როდესაც and ოპერატორს ვიყენებთ - უპერატესობა ენიჭება False-ს
+
+# or - ან
+print(True or True) # output: True
+print(True or False) # output: True
+print(False or True) # output: True
+print(False or False) # output: True
+
