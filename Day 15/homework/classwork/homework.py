28b3344dc57baf931d5363e010489d98f2225357
demetresuxitashvili <tamunichko@gmail.com>
2025-05-28 20:31:55 +0400

    commit

--- Day 15/homework/classwork/homework.py
+++
@@ -1,8 +0,0 @@
-# 1) input() 2) input() 3) # 4) f
-
-# 2
-user_name = input("type name:")
-sure_name = input("type surename:")
-number = input("type number:")
-adrees = input("type adress:")
-time = input("type time")
