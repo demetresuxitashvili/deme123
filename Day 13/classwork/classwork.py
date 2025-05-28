4f73735513edbe813e3cc3b0ebfa4f8dd72b34b8
demetresuxitashvili <tamunichko@gmail.com>
2025-05-28 21:37:29 +0400

    second commit

--- Day 13/classwork/classwork.py
+++ Day 13/classwork/classwork.py
@@ -1,22 +1,3 @@
-# == < > = != <= >=
+#<!-- ატრიბუტი არის თვისება -->
+# <!-- <img> ფოტოს იტანს. <a> ლინკის იტანს. -->
 
-num1 = 30
-num2 = input("enter number: ")
-print(num1 == num2)
-
-num1 = 30
-num2 = input("enter number: ")
-print(num1 != num2)
-
-num1 = 30
-num2 = input("ebter number: ")
-print(num1 < num2)
-
-Num1 = 30
-num2 = input("enter number: ")
-print(Num1 > num2)
-
-soil_moisture = 80
-limit = 100
-print(soil_moisture != limit)
-# ამ კოდმა გამოიტანა True რადგან 80 არ ემთხვევა 100
