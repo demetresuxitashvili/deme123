ca80f22d2cbcda374f204121cd45c4740bf2cf15
demetresuxitashvili <tamunichko@gmail.com>
2025-06-14 10:46:53 +0400

    second commit

---
+++ Day 20/homework/homework.py
@@ -0,0 +1,16 @@
+surename = input("enter your surename:")
+
+for sure in surename:
+    print(surename)
+
+for i in range(50, 19, -3):
+    print(i)
+
+
+number = 150
+
+while number >= 0:
+    print(number)
+    number -= 1
+
+# while loop-სრულდება თუ არ იცი და სანამ პირობა false რ გახდება მისი მუშაობა გრძელდება
