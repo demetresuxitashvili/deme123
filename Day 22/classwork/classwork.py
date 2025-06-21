066ba67bb7996b130b13e56504663aad4182f34e
demetresuxitashvili <tamunichko@gmail.com>
2025-06-21 18:43:39 +0400

    commit

--- Day 22/classwork/classwork.py
+++ Day 22/classwork/classwork.py
@@ -1,6 +1,12 @@
 # if-ის დანიშნულება არის, თუ, შედარების ოპერატორი სადაც შეგიძლია დაწერო.  elif-ის რომ დაამატო კიდევ ერთი შედარების ოპერატორი. else-ის  სხვა შემთხვევაში, თუ if-ის და elif-ის პასუხი არ იყო სწორი else-ს პასუხი იქნება სწორი
 
-deme = "sukhitashvili"
+me = "sukhitashvili"
 
-input("enter your surename: ")
+user = input("enter your surename: ")
+
+if me == user:
+    print("Our surnames are similar")
+
+else:
+    print("Our surnames are not similar")
 
