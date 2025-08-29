2fdd5f2629138b91e766423530e482641cf0a847
demetresuxitashvili <tamunichko@gmail.com>
2025-08-29 16:29:19 +0400

    commit

---
+++ Day 55/homework/homework.py
@@ -0,0 +1,10 @@
+# 1) მოიფიქრეთ ამ ამოცანის ამოხსნის დამატებითი გზები, Solution-ებში ჩახედვის გარეშე;)
+# https://www.codewars.com/kata/59441520102eaa25260000bf/train/python
+
+# 2) https://www.codewars.com/kata/50654ddff44f800200000004/train/python
+
+# 3) https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/python
+
+# 4) https://www.codewars.com/kata/5715eaedb436cf5606000381/train/python
+
+# 5) https://www.codewars.com/kata/5865918c6b569962950002a1/train/python
