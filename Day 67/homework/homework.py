a424b0d301a376dd70b783cf506a2edcb9bec4f2
demetresuxitashvili <tamunichko@gmail.com>
2025-09-29 22:01:13 +0400

    commit

--- Day 67/homework/homework.py
+++ Day 67/homework/homework.py
@@ -12,13 +12,18 @@ print(reverse_str(['banana', 'apple', 'strawbery']))
 
 # 3) შექმენით ფუნქცია double, რომელიც მიიღებს სიას და დააბრუნებს ახალ სიას, სადაც ყველა რიცხვი გაორმაგებულია.
 def double(double_list):
-    return double_list * 2
+    list = []
+    for i in double_list:
+        list.append(i * 2)
+    return list
 
 print(double([1, 2, 3, 4]))
 
 # 4) დაწერე ფუნქცია sum_of_digits, რომელიც მიიღებს რიცხვს და დააბრუნებს მის ციფრთა ჯამს (მაგ. 123 → 6).
 def sum_of_digits(digits):
-    return digits + digits
+    sum = 0
+    for i in str(digits):
+        sum + int(i)
 
 print(sum_of_digits(123))
 
@@ -26,7 +31,14 @@ print(sum_of_digits(123))
 #    მაგ. მოცემულია რიცხვი 4. 4-ის გამყოფებია: 1, 2, 4. შესაბამისად ამ რიცხვის გამყოფებს აბრუნებთ სიის სახით --> [1, 2, 4]
 #    12 -> [1, 2, 3, 4, 6, 12]
 #    45 -> [1, 3, 5, 9, 15, 45]
-#  ვერ გავიგე
+def divisors(num):
+    bla = []
+    for i in range(1, num+1):
+        if num % i == 0:
+            bla.append(i)
+    return bla
+
+print(divisors([12]))
 
 # 6) შექმენით ფუნქცია max, რომელსაც არგუმენტად გადასცემთ რიცხვების სიას და პასუხად დააბრუნებს ამ რიცხვებიდან მაქსიმალურს.
 def max(max_list):
