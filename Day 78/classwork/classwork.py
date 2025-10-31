67ea234abeb97ca3685772fb1219a5aa596eda2b
demetresuxitashvili <tamunichko@gmail.com>
2025-10-31 22:11:25 +0400

    'commit'

---
+++ Day 78/classwork/classwork.py
@@ -0,0 +1,13 @@
+# 8kyu
+# ----
+# 1) https://www.codewars.com/kata/54147087d5c2ebe4f1000805/train/python
+# პირობა ვერ გავიგე
+# 2) https://www.codewars.com/kata/56676e8fabd2d1ff3000000c/train/python
+# პირობა ვერ გავიგე
+# 3) https://www.codewars.com/kata/51f9d93b4095e0a7200001b8/train/python
+# გავაკეთე
+
+# 7kyu
+# ----
+# 4) https://www.codewars.com/kata/5502c9e7b3216ec63c0001aa/train/python
+# პირობა ვერ გავიგე
